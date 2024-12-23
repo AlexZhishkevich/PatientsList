5bb434afe3918031de37c48e18293726e29ae8fa
Alexey Zhishkevich <lexa2669847@gmail.com>
2024-12-23 23:00:11 +0300

    feat: + comments

--- src/Core/PatientsList.Domain/Models/People/NameData.cs
+++ src/Core/PatientsList.Domain/Models/People/NameData.cs
@@ -4,10 +4,20 @@
     {
         public Guid Id { get; set; }
 
+        /// <summary>
+        /// Identifies the purpose for this name.
+        /// </summary>
         public NameUsageType? Use { get; set; }
 
+        /// <summary>
+        /// Family name (often called 'Surname')
+        /// </summary>
         public string Family { get; set; } = string.Empty;
 
+        /// <summary>
+        /// Given names (not always 'first'). Includes middle names
+        /// <br/>This repeating element order: Given Names appear in the correct order for presenting the name
+        /// </summary>
         public List<string>? GivenNames { get; set; } = new();
 
         public static NameData Default => new() { Use = NameUsageType.Anonymous };
--- src/Core/PatientsList.Domain/Models/People/Patient.cs
+++ src/Core/PatientsList.Domain/Models/People/Patient.cs
@@ -1,14 +1,34 @@
 ﻿namespace PatientsList.Domain.Models.People
 {
+    /// <summary>
+    /// Demographics and other administrative information about an individual that is the subject of potential, past, current, or future health-related care, services, or processes.
+    /// </summary>
     public class Patient
     {
+        /// <summary>
+        /// An identifier for this patient.
+        /// </summary>
         public Guid Id { get; set; }
+
+        /// <summary>
+        /// A name associated with the individual.
+        /// </summary>
         public NameData Name { get; set; } = NameData.Default;
 
+        /// <summary>
+        /// The gender that the patient is considered to have for administration and record keeping purposes.
+        /// </summary>
         public GenderType? Gender { get; set; }
 
+        /// <summary>
+        /// The date of birth for the individual.
+        /// </summary>
         public DateTime BirthDate { get; set; }
 
+        /// <summary>
+        /// Property, that determines, whether this patient record is in active use. 
+        /// <br/>Many systems use this property to mark as non-current patients, such as those that have not been seen for a period of time based on an organization's business rules.
+        /// </summary>
         public bool? Active { get; set; }
     }
 }
